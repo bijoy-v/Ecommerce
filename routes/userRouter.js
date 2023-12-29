f90d7159ab8e46d73b5684ccc871ab67069d1e08
bijoy-v <bijoyyv@gmail.com>
2023-12-29 12:10:05 +0530

    firt commit

---
+++ routes/userRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const userRouter = express.Router();
+
+/* GET users listing. */
+userRouter.get('/', function(req, res, next) {
+  res.render("user/index",{title:"welcome"})
+});
+
+userRouter.get('/about', function(req, res, next) {
+  res.render("user/about",{title:"about",layout:"layouts/sidebar"})
+});
+
+module.exports = userRouter;
