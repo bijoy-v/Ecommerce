f9da5450f6d8f19709ba4515cc6174f9d8acff4f
bijoy-v <bijoyyv@gmail.com>
2024-01-02 18:34:55 +0530

    user

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -7,4 +7,25 @@ adminRouter.get('/', function(req, res, next) {
   res.render("admin/dashboard",{title:"Admin Here", layout:"layouts/admin-layout.ejs"})
 });
 
+adminRouter.get('/products', function(req, res, next) {
+  try{
+  res.render("admin/adminProducts",{title:"Admin Here", layout:"layouts/admin-layout.ejs"})
+  }catch (error){
+    console.log(error);
+  }
+});
+adminRouter.get('/users', function(req, res, next) {
+  res.render("admin/admin-users",{title:"Admin Here", layout:"layouts/admin-layout.ejs"})
+});
+
+adminRouter.get('/profile', function(req, res, next) {
+  res.render("admin/adminProfile",{title:"Admin Here", layout:"layouts/admin-layout.ejs"})
+});
+
+adminRouter.get('/orders', function(req, res, next) {
+  res.render("admin/adminOrder",{title:"Admin Here", layout:"layouts/admin-layout.ejs"})
+});
+
+
+
 module.exports = adminRouter;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,6 +10,17 @@ userRouter.get('/about', function(req, res, next) {
   res.render("user/about",{title:"about",layout:"layouts/sidebar"})
 });
 
+userRouter.get('/product', function(req, res, next) {
+  res.render("user/userProducts",{title:"about",layout:"layouts/layout"})
+});
+
+userRouter.get('/cart', function(req, res, next) {
+  res.render("user/userCart",{title:"about",layout:"layouts/layout"})
+});
+
+userRouter.get('/wishlist', function(req, res, next) {
+  res.render("user/userWishlist",{title:"about",layout:"layouts/layout"})
+});
 
 
 module.exports = userRouter;
